1de92803398311e0bd307d74d7cd6f3d6b280d33
Felipe Barros <felipelfbarros@gmail.com>
2021-01-28 12:36:41 -0300

    Move gsheets API calls to a pipeline

--- vacinometro/settings.py
+++ vacinometro/settings.py
@@ -4,3 +4,7 @@ SPIDER_MODULES = ["vacinometro.spiders"]
 NEWSPIDER_MODULE = "vacinometro.spiders"
 
 ROBOTSTXT_OBEY = True
+
+ITEM_PIPELINES = {
+    "vacinometro.pipelines.SyncVaccinesDataToGoogleSheetsPipeline": 500
+}
--- vacinometro/spiders/vaccines.py
+++ vacinometro/spiders/vaccines.py
@@ -1,9 +1,5 @@
 import re
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import scrapy
-import json
-import os
 
 
 class VaccinesSpider(scrapy.Spider):
@@ -33,12 +29,6 @@ class VaccinesSpider(scrapy.Spider):
         total_vaccinated = int(total_vaccinated.replace(".", ""))
         total_vaccinated_today = int(total_vaccinated_today.replace(".", ""))
 
-        sheet = get_gsheet()
-        update_sheet_headers(sheet, vaccines)
-        row = [last_vaccinated_date, total, total_vaccinated, total_vaccinated_today]
-        row.extend([value for vaccine in vaccines for value in vaccine.values()])
-        sheet.append_row(row)
-
         return {
             "total": total,
             "total_de_vacinados": total_vaccinated,
@@ -46,29 +36,3 @@ class VaccinesSpider(scrapy.Spider):
             "data": last_vaccinated_date,
             "vacinas": vaccines,
         }
-
-
-def get_gsheet():
-    scope = [
-        "https://www.googleapis.com/auth/drive",
-        "https://spreadsheets.google.com/feeds"
-    ]
-    key_file_dict = json.loads(os.getenv("GOOGLE_CREDENTIALS"))
-    credentials = ServiceAccountCredentials.from_json_keyfile_dict(key_file_dict, scope)
-
-    gc = gspread.authorize(credentials)
-    return gc.open_by_key(os.getenv("GOOGLE_SHEET_ID")).sheet1
-
-
-def update_sheet_headers(sheet, vaccines):
-    sheet.update_cell(1, 1, 'Data')
-    sheet.update_cell(1, 2, 'Total de Vacinas')
-    sheet.update_cell(1, 3, 'Total de Vacinados')
-    sheet.update_cell(1, 4, 'Total de Vacinados no Dia')
-    offset = 1
-    for vaccine in vaccines:
-        vaccine_name = list(vaccine.keys())[0]
-        vaccine_name = vaccine_name.split("_")
-        vaccine_name = " ".join(list(map(lambda text: text.title(), vaccine_name)))
-        sheet.update_cell(1, 4 + offset, vaccine_name)
-        offset += 1
