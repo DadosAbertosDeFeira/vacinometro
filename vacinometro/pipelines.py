1b9ea767fe5eb8366b9dacd7dc22d05e6ec8ffba
Felipe Barros <felipelfbarros@gmail.com>
2021-02-08 18:08:07 -0300

    Adiciona data e horário da coleta (#11)

--- vacinometro/pipelines.py
+++ vacinometro/pipelines.py
@@ -3,6 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 import os
 import json
+from datetime import datetime, timedelta, timezone
 
 
 class FakeWorksheet:
@@ -34,25 +35,38 @@ class SyncVaccinesDataToGoogleSheetsPipeline:
 
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
+        crawl_datetime = (
+            datetime.utcnow()
+            .replace(tzinfo=timezone.utc)
+            .astimezone(tz=timezone(offset=timedelta(hours=-3)))
+            .strftime("%d/%m/%Y %H:%M:%S")
+        )
         last_vaccinated_date = adapter.get("data")
         total = adapter.get("total")
         total_vaccinated = adapter.get("total_de_vacinados")
         total_vaccinated_today = adapter.get("total_de_vacinados_hoje")
         vaccines = adapter.get("vacinas")
         self.update_sheet_headers(vaccines)
-        row = [last_vaccinated_date, total, total_vaccinated, total_vaccinated_today]
+        row = [
+            crawl_datetime,
+            last_vaccinated_date,
+            total,
+            total_vaccinated,
+            total_vaccinated_today,
+        ]
         row.extend([value for vaccine in vaccines for value in vaccine.values()])
         self.worksheet.append_row(row)
 
     def update_sheet_headers(self, vaccines):
-        self.worksheet.update_cell(1, 1, "Data")
-        self.worksheet.update_cell(1, 2, "Total de Vacinas")
-        self.worksheet.update_cell(1, 3, "Total de Vacinados")
-        self.worksheet.update_cell(1, 4, "Total de Vacinados no Dia")
+        self.worksheet.update_cell(1, 1, "Data e Horário da Coleta")
+        self.worksheet.update_cell(1, 2, "Data")
+        self.worksheet.update_cell(1, 3, "Total de Vacinas")
+        self.worksheet.update_cell(1, 4, "Total de Vacinados")
+        self.worksheet.update_cell(1, 5, "Total de Vacinados no Dia")
         offset = 1
         for vaccine in vaccines:
             vaccine_name = list(vaccine.keys())[0]
             vaccine_name = vaccine_name.split("_")
             vaccine_name = " ".join(list(map(lambda text: text.title(), vaccine_name)))
-            self.worksheet.update_cell(1, 4 + offset, vaccine_name)
+            self.worksheet.update_cell(1, 5 + offset, vaccine_name)
             offset += 1
